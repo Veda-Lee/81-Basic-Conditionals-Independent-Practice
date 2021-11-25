3b22270d38b3070186a1549fb4624933c684aa62
Veda-Lee <78391451+Veda-Lee@users.noreply.github.com>
2021-11-25 15:20:10 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,55 @@
+// 1. English Section
+//All the sections work except English section for some reason
+$(".english-button").click(function() {
+    let englishInput = $(".english").val();
+    if (englishInput === "Mon") {
+        $(".english-message").text("Correct!");
+        $(".english").css("color", "green");
+    } else{
+        $(".english-message").text("Wrong! Try again.");
+        $(".english").css("color", "red");
+    }
+
+});
+
+// 2. Social Studies section
+$(".ss-button").click(function() {
+    let ssInput = $(".social-studies").val();
+    if (ssInput === "Albany") {
+        $(".ss-message").text("Correct!");
+        $(".social-studies").css("color", "green");
+    } else {
+        $(".ss-message").text("Wrong! Try again.");
+        $(".social-studies").css("color", "red");
+    }
+
+});
+
+
+// 3. Math section
+$(".math-button").click(function() {
+    let mathInput = Number($(".math").val());
+
+    if (mathInput === 30) {
+        $(".math-message").text("Correct!");
+        $(".math").css("color", "green");
+    } else {
+        $(".math-message").text("Wrong! Try again.");
+        $(".math").css("color", "red");
+    }
+
+});
+
+
+// 4. Science section
+$(".science-button").click(function() {
+    let scienceInput = $(".science").val();
+    if (scienceInput === "solid") {
+        $(".science-message").text("Correct!");
+        $(".science").css("color", "green");
+    } else {
+        $(".science-message").text("Wrong! Try again.");
+        $(".science").css("color", "red");
+    }
+
+});
